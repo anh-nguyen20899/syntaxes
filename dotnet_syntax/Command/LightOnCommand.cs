6a7859876d492da82a3109066d6b978aee0bb6d5
anhng <anananh1999@gmail.com>
2023-10-04 22:25:25 +1100

    Modify command pattern

---
+++ dotnet_syntax/Command/LightOnCommand.cs
@@ -0,0 +1,20 @@
+public class LightOnCommand : Command {
+ Light light;
+
+    public void execute()
+    {
+        throw new NotImplementedException();
+    }
+
+    //  public LightOnCommand(Light light) {
+    //  this.light = light;
+    //  }
+    //  public void execute() {
+    //  light.on();
+    //  }
+}
+
+internal class Light
+{
+    
+}
